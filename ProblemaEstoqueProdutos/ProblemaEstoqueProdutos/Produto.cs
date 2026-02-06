834947e70843408d0570199c0c469bab7f809bb2
samuelfrancis03 <samuelfrancis03@hotmail.com>
2026-02-06 11:35:17 -0300

    fifth commit

---
+++ ProblemaEstoqueProdutos/ProblemaEstoqueProdutos/Produto.cs
@@ -0,0 +1,47 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using System.Globalization;
+
+namespace ProblemaEstoqueProdutos
+{
+    internal class Produto
+    {
+        public string Nome { get; set; }
+        public double Preco { get; set; }
+        public int Quantidade { get; set; }
+
+        public double ValorTotalEmEstoque() 
+        {
+            return Preco * Quantidade;
+        }
+
+        public void AdicionarProdutos(int quantidade) 
+        {
+            Quantidade += quantidade;
+        }
+
+        public void RemoverProdutos(int quantidade) 
+        {
+            Quantidade -= quantidade;
+        }
+        
+        /*
+         Converte o objeto para string
+         Sobrepondo o ToString()
+         */
+        public override string ToString()
+        {
+            return Nome
+                + ", $ "
+                + Preco.ToString("F2", CultureInfo.InvariantCulture) 
+                + ", "
+                + Quantidade
+                +" unidades, Total: $ "
+                + ValorTotalEmEstoque().ToString("F2", CultureInfo.InvariantCulture);
+        }
+
+
+
+    }
+}
---
+++ ProblemaEstoqueProdutos/ProblemaEstoqueProdutos/Program.cs
@@ -0,0 +1,35 @@
+﻿
+using ProblemaEstoqueProdutos;
+using System.Globalization;
+
+Produto produto = new Produto();
+
+Console.WriteLine("Entre com os dados do produto: ");
+Console.Write("Nome: ");
+produto.Nome = Console.ReadLine();
+Console.Write("Preco: ");
+produto.Preco = double.Parse(Console.ReadLine(), CultureInfo.InvariantCulture);
+Console.Write("Quantidade: ");
+produto.Quantidade = int.Parse(Console.ReadLine(), CultureInfo.InvariantCulture);
+Console.WriteLine();
+
+/*Com a sobreposição do ToString(), o progama já detecta a função da
+classe e converte o objeto conforme ele foi formatado*/
+Console.WriteLine("Dados do produto: " + produto);
+Console.WriteLine();
+
+Console.Write("Digite o número de produtos a ser adicionado: ");
+int qtde = int.Parse(Console.ReadLine());
+produto.AdicionarProdutos(qtde);
+Console.WriteLine();
+
+Console.WriteLine("Dados atualizados: " + produto);
+Console.WriteLine();
+
+Console.Write("Digite o número de produtos a ser removidos: ");
+qtde = int.Parse(Console.ReadLine());
+
+produto.RemoverProdutos(qtde);
+Console.WriteLine();
+
+Console.WriteLine("Dados atualizados: " + produto);
