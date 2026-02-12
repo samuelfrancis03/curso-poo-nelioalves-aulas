45b3484c14ab707ac83a9de84e4e60aea6983bda
samuelfrancis03 <samuelfrancis03@hotmail.com>
2026-02-12 17:43:17 -0300

    Exercicio Vetores de classes

---
+++ ExercicioVetores/ExercicioVetores/Program.cs
@@ -0,0 +1,35 @@
+﻿
+using ExercicioVetores;
+
+Estudante[] quartosAlugado =  new Estudante[10];
+var cont = 1;
+
+Console.Write("Quantos quartos serão alugados: ");
+int n = int.Parse(Console.ReadLine());
+Console.WriteLine();
+
+
+for (int i = 0; i < n; i++) 
+{
+    Console.WriteLine("Alguel #" + cont++ + ":");
+    Console.Write("Nome: ");
+    string nome = Console.ReadLine();
+    Console.Write("Email: ");
+    string email = Console.ReadLine();
+    Console.Write("Quarto: ");
+    int quarto = int.Parse(Console.ReadLine());
+    Console.WriteLine();
+
+    quartosAlugado[quarto] = new Estudante(nome, email, quarto);
+}
+
+Console.WriteLine("Quartos ocupados: ");
+for (int i = 0; i < quartosAlugado.Length; i++) 
+{
+    if (quartosAlugado[i] != null) 
+    {
+        
+        Console.WriteLine(quartosAlugado[i]);
+
+    }
+}
