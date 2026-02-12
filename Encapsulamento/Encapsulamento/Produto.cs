f3416fdfc7f1bd3d492ed5be348c9425a10327c0
samuelfrancis03 <samuelfrancis03@hotmail.com>
2026-02-12 10:35:59 -0300

    Aulas sobre Encapsulamento

---
+++ Encapsulamento/Encapsulamento/Produto.cs
@@ -0,0 +1,107 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using System.Globalization;
+
+namespace Encapsulamento
+{
+    internal class Produto
+    {
+        /*
+        Ordem para implementação de memmbros da classe
+            -Atributos privados
+            -Propiedades autoimplementantes
+            -Construtores
+            -Propiedades custumizadas
+            -Outros métodos da classe
+        */
+
+        //Padrão de nome, para atributos privados: '_nomeVariavel'
+        private string _nome;
+        private int _quantidade;
+        public double Preco { get; private set; } //AutoPropierties
+
+        //Construtor padrão - possibilita a instanciação sem parametros
+        public Produto()
+        {
+        }
+
+        //Construtor sobrecarga, mesmo nome porém assinatura diferentes
+        public Produto(string nome, double preco) : this() //referencia o construtor padrão
+        {
+            _nome = nome;
+            Preco = preco;
+        }
+
+        //Construtor
+        public Produto(string nome, double preco, int quantidade) : this(nome, preco) //referencia o construtor com nome e preço do anterior
+        {
+            _nome = nome;
+            Preco = preco;
+            _quantidade = quantidade;
+        }
+
+        //Metodos GET e SET manual
+
+        public string GetNome() 
+        {
+            return _nome;
+        }
+
+        public void SetNome(string nome) 
+        {
+            if (nome != null && nome.Length > 1) 
+            {
+                _nome = nome; 
+            }
+            
+        }
+
+
+        //Properties manual
+        public int Quantidade
+        {
+            get { return _quantidade; }
+            set
+            {
+                if (value != null && value > 0)
+                {
+                    _quantidade = value;
+                }
+            }
+        }
+
+        public double ValorTotalEmEstoque()
+        {
+            return Preco * _quantidade;
+        }
+
+        public void AdicionarProdutos(int quantidade)
+        {
+            _quantidade += quantidade;
+        }
+
+        public void RemoverProdutos(int quantidade)
+        {
+            _quantidade -= quantidade;
+        }
+
+        /*
+         Converte o objeto para string
+         Sobrepondo o ToString()
+         */
+        public override string ToString()
+        {
+            return _nome
+                + ", $ "
+                + Preco.ToString("F2", CultureInfo.InvariantCulture)
+                + ", "
+                + _quantidade
+                + " unidades, Total: $ "
+                + ValorTotalEmEstoque().ToString("F2", CultureInfo.InvariantCulture);
+        }
+
+
+
+    }
+}
---
+++ Encapsulamento/Encapsulamento/Program.cs
@@ -0,0 +1,14 @@
+﻿using Encapsulamento;
+
+Produto produto = new Produto("TV", 500.00, 10);
+
+//acessando atraves de metodos GET e SET manual
+produto.SetNome("TV 4K");
+Console.WriteLine(produto.GetNome());
+
+//acessando atraves de Auto Propierties manual
+Console.WriteLine(produto.Preco);
+
+//acessando atraves de Propierties manual
+produto.Quantidade = 0;
+Console.WriteLine(produto.Quantidade);
