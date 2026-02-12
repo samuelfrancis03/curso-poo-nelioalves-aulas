2b1dc94edc9acece6fa044df3d485e20c3d97817
samuelfrancis03 <samuelfrancis03@hotmail.com>
2026-02-12 10:35:12 -0300

    Aulas sobre construtores

---
+++ Construtores/Construtores/Produto.cs
@@ -0,0 +1,69 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using System.Globalization;
+
+namespace ProblemaEstoqueProdutos
+{
+    internal class Produto
+    {
+        public string Nome { get; set; }
+        public double Preco { get; set; }
+        public int Quantidade { get; set; }
+
+        //Construtor padrão - possibilita a instanciação sem parametros
+        public Produto() {
+            Quantidade = 10;
+        }
+
+        //Construtor sobrecarga, mesmo nome porém assinatura diferentes
+        public Produto(string nome, double preco) : this() //referencia o construtor padrão
+        {
+            Nome = nome;
+            Preco = preco;
+        }
+
+        //Construtor
+        public Produto(string nome, double preco, int quantidade) : this(nome, preco) //referencia o construtor com nome e preço do anterior
+        {
+            Nome = nome;
+            Preco = preco;
+            Quantidade = quantidade;
+        }
+
+
+
+        public double ValorTotalEmEstoque()
+        {
+            return Preco * Quantidade;
+        }
+
+        public void AdicionarProdutos(int quantidade)
+        {
+            Quantidade += quantidade;
+        }
+
+        public void RemoverProdutos(int quantidade)
+        {
+            Quantidade -= quantidade;
+        }
+
+        /*
+         Converte o objeto para string
+         Sobrepondo o ToString()
+         */
+        public override string ToString()
+        {
+            return Nome
+                + ", $ "
+                + Preco.ToString("F2", CultureInfo.InvariantCulture)
+                + ", "
+                + Quantidade
+                + " unidades, Total: $ "
+                + ValorTotalEmEstoque().ToString("F2", CultureInfo.InvariantCulture);
+        }
+
+
+
+    }
+}
---
+++ Construtores/Construtores/Program.cs
@@ -0,0 +1,48 @@
+﻿
+using ProblemaEstoqueProdutos;
+using System.Globalization;
+
+
+
+
+Console.WriteLine("Entre com os dados do produto: ");
+Console.Write("Nome: ");
+string nome = Console.ReadLine();
+Console.Write("Preco: ");
+double preco = double.Parse(Console.ReadLine(), CultureInfo.InvariantCulture);
+Console.WriteLine();
+
+//Instanciando construtor com sobrecarga
+Produto produto = new Produto(nome, preco);
+
+//Instanciando construtor padrao
+Produto produto2 = new Produto();
+
+//Instanciando objeto, sem necessida de construtor
+Produto produto3 = new Produto() 
+{ 
+    Nome = "Tv", 
+    Preco = 500.00, 
+    Quantidade = 20
+};
+
+/*Com a sobreposição do ToString(), o progama já detecta a função da
+classe e converte o objeto conforme ele foi formatado*/
+Console.WriteLine("Dados do produto: " + produto);
+Console.WriteLine();
+
+Console.Write("Digite o número de produtos a ser adicionado: ");
+int qtde = int.Parse(Console.ReadLine());
+produto.AdicionarProdutos(qtde);
+Console.WriteLine();
+
+Console.WriteLine("Dados atualizados: " + produto);
+Console.WriteLine();
+
+Console.Write("Digite o número de produtos a ser removidos: ");
+qtde = int.Parse(Console.ReadLine());
+
+produto.RemoverProdutos(qtde);
+Console.WriteLine();
+
+Console.WriteLine("Dados atualizados: " + produto);
