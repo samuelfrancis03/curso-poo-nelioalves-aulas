f3147cd07631779ebceb9b5dc738918a22b279e5
samuelfrancis03 <samuelfrancis03@hotmail.com>
2026-02-25 10:05:13 -0300

    Aula de Modificadores ref e out

---
+++ ModificadorDeParametros/ModificadorDeParametros/Calculator.cs
@@ -0,0 +1,65 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace ModificadorDeParametros
+{
+    internal class Calculator
+    {
+        /*
+        //usando sobrecarga - jeito ruim
+        public static int Sum(int n1, int n2) 
+        {
+            return n1 + n2;
+        }
+
+        public static int Sum(int n1, int n2, int n3)
+        {
+            return n1 + n2 + n3;
+        }
+
+        public static int Sum(int n1, int n2, int n3, int n4)
+        {
+            return n1 + n2 + n3 + n4;
+        }
+        */
+
+        //usando vetor
+        public static int Sum(int[] numbers)
+        {
+            int sum = 0;
+            for (int i = 0; i < numbers.Length; i++)
+            {
+                sum += numbers[i];
+            }
+            return sum;
+        }
+
+        //usando o modificador de parametros 'params'
+        //Descarta a necessidade de instanciar o vetor na main, para passar os valores
+        public static int SuParams(params int[] numbers)
+        {
+            int sum = 0;
+            for (int i = 0; i < numbers.Length; i++)
+            {
+                sum += numbers[i];
+            }
+            return sum;
+        }
+
+        //usando o modificador de parametro 'ref'
+        //O parametro ref da função vai torna uma referencia da variavel original
+        public static void Triple(ref int x) 
+        {
+            x = x * 3;
+        }
+
+        //O modificar ou é similar ao ref, mas não ecige que a variavel original seja iniciada
+        //e podemos armazenar em outra variavel de saida
+        public static void Dobro(int origin, out int result) 
+        {
+            result = origin * 2;
+        }
+
+    }
+}
---
+++ ModificadorDeParametros/ModificadorDeParametros/Program.cs
@@ -0,0 +1,22 @@
+﻿
+using ModificadorDeParametros;
+
+int s1 = Calculator.Sum(new int[] { 2, 3 });
+int s2 = Calculator.Sum(new int[] { 2, 3, 4});
+Console.WriteLine(s1);
+Console.WriteLine(s2);
+
+
+int sp1 = Calculator.SuParams(2, 3);
+int sp2 = Calculator.SuParams(2, 3, 4);
+Console.WriteLine(sp1);
+Console.WriteLine(sp2);
+
+int a = 10;
+Calculator.Triple(ref a);
+Console.WriteLine(a);
+
+a = 10;
+int dobro;
+Calculator.Dobro(a, out dobro);
+Console.WriteLine(dobro);
