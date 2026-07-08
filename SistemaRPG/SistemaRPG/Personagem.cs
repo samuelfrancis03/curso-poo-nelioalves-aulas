fa5e6b48638d699af99e6dbf1c1b4a42c1a900d6
samuelfrancis03 <samuelfrancis03@hotmail.com>
2026-07-08 19:29:34 -0300

    SistemaRPG 1.0 - Aplicando Conhecimentos Modulo 4

---
+++ SistemaRPG/SistemaRPG/Personagem.cs
@@ -0,0 +1,78 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Security.Principal;
+using System.Text;
+
+namespace SistemaRPG
+{
+    internal class Personagem
+    {
+        public string Nome { get; set; }
+        public string Classe { get; set; }
+        public int Vida { get; set; }
+        public int Ataque { get; set; }
+        public int Defesa { get; set; }
+        public static int TotalPersonagem { get; set; }
+        public void ReceberDano(int dano) 
+        {
+            int totalVida = Vida - dano;
+
+            if (totalVida >= 0) 
+            {
+                Vida = totalVida;
+            }
+            
+        }
+
+        public void Curar(int cura) 
+        {
+           int totalVida = Vida + cura;
+
+            if (totalVida <= 100) 
+            {
+                Vida = totalVida;
+            }
+
+        }
+
+        public bool EstaVivo() 
+        {
+            if (Vida > 0) 
+            {
+                return true;
+                //Vivo
+            }
+            else 
+            {
+                return false;
+                //Derrotado
+            }
+        }
+
+        public int CalcularPoder() 
+        {
+            return Ataque + Defesa + Vida;
+        }
+
+        public static void AdicionarPersonagem() 
+        {
+            TotalPersonagem++;
+        }
+
+        public override string ToString()
+        {
+            //Convertendo o resultado bool, para em forma de string com if ternário
+            string status = EstaVivo() ? "Vivo" : "Derrotado";
+
+            return "Nome: " + Nome  
+                + "\nClasse: " + Classe
+                + "\nVida: " + Vida
+                + "\nAtaque: " + Ataque
+                + "\nDefesa: " + Defesa
+                + "\nPoder Total: " + CalcularPoder()
+                + "\nStatus: " + status;
+        }
+
+
+    }
+}
---
+++ SistemaRPG/SistemaRPG/Program.cs
@@ -0,0 +1,41 @@
+﻿using SistemaRPG;
+
+Personagem personagem = new Personagem();
+
+Console.Write("Nome do personagem: ");
+personagem.Nome = Console.ReadLine();
+Console.Write("Classe: ");
+personagem.Classe = Console.ReadLine();
+Console.Write("Vida inicial: ");
+personagem.Vida = int.Parse(Console.ReadLine());
+Console.Write("Ataque: ");
+personagem.Ataque = int.Parse(Console.ReadLine());
+Console.Write("Defesa: ");
+personagem.Defesa = int.Parse(Console.ReadLine());
+Personagem.AdicionarPersonagem(); //mebros estaticos chamamos pela Classe, não pelo o objeto
+
+
+Console.WriteLine();
+Console.WriteLine(personagem);
+
+
+var entrada = 0;
+Console.WriteLine();
+Console.Write("Quanto de dano o personagem recebeu? ");
+entrada = int.Parse(Console.ReadLine());
+personagem.ReceberDano(entrada);
+
+Console.WriteLine();
+Console.WriteLine(personagem);
+
+Console.Write("Quanto deseja curar? ");
+entrada = int.Parse(Console.ReadLine());
+personagem.Curar(entrada);
+
+Console.WriteLine();
+Console.WriteLine(personagem);
+Console.WriteLine();
+Console.WriteLine("Total de personagens criados: " + Personagem.TotalPersonagem);
+
+
+
