1bbfd1e0f9ac1ea045d84c9d6d865515cff1fe5f
samuelfrancis03 <samuelfrancis03@hotmail.com>
2026-07-14 17:02:18 -0300

    Aula sobre estrutura de dados - Vetor, Listas e Matrizes

--- EstruturaDeDados/EstruturaDeDados/Product.cs
+++ EstruturaDeDados/EstruturaDeDados/Product.cs
@@ -8,5 +8,13 @@ namespace EstruturaDeDados
     {
         public string Name { get; set; }
         public double Price { get; set; }
+
+        public Product(string name, double price)
+        {
+            Name = name;
+            Price = price;
+        }
     }
+
+            
 }
