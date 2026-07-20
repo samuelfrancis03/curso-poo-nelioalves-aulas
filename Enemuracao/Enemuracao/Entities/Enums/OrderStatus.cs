017d774af980ec86f99c894ad7d768e57cad4059
samuelfrancis03 <samuelfrancis03@hotmail.com>
2026-07-20 16:20:30 -0300

    Aula Enumeração

--- Enemuracao/Enemuracao/Entities/Enums/OrderStatus.cs
+++ Enemuracao/Enemuracao/Entities/Enums/OrderStatus.cs
@@ -6,6 +6,7 @@ namespace Enemuracao.Entities.Enums
 {
     enum OrderStatus : int
     {
+        /*Não é necessário informar o o numero, pois com apenas o nome o compilador já reconhece o 'id'*/
         PendingPayment = 0,
         Processing = 1,
         Shipped = 2,
