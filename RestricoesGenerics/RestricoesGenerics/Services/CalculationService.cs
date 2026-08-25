8a814994f1c3e6556b10fde168043e7fb5d1a011
samuelfrancis03 <samuelfrancis03@hotmail.com>
2026-08-25 12:33:38 -0300

    Aula sobre Restrições do Genercis

---
+++ RestricoesGenerics/RestricoesGenerics/Services/CalculationService.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace RestricoesGenerics.Services
+{
+    internal class CalculationService
+    {
+        //Metodo Generico
+        //Temos que usar o IComparable para ter a comparação entre objetos.
+        public T Max<T>(List<T> list) where T : IComparable
+        {
+            if (list.Count == 0) 
+            {
+                throw new ArgumentException("The list can not be empty");
+            }
+
+            T max = list[0];
+            for (int i = 1; i < list.Count; i++) {
+                if (list[i].CompareTo(max) > 0) {
+                    max = list[i];
+                }
+
+            }
+            return max;
+        }
+
+    }
+}
