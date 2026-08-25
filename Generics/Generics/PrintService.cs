91461c7de37d0d9546ce5592ee303debb6da4db9
samuelfrancis03 <samuelfrancis03@hotmail.com>
2026-08-25 12:32:50 -0300

    Aula sobre Generics

---
+++ Generics/Generics/PrintService.cs
@@ -0,0 +1,49 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Generics
+{
+    internal class PrintService<T>
+    {
+        // O tipo fica dinamico, ou seja, de acordo que for instaciado na progaman , vai ser tipo do Type Safety
+
+        private T[] _values = new T[10];
+        private int _count = 0;
+
+        public void AddValue(T value) 
+        {
+            if (_count == 10) 
+            {
+                throw new InvalidOperationException("PrintService is full");
+            }
+            _values[_count]  = value;
+            _count++;
+        }
+
+        public T First() 
+        {
+            if (_count == 0)
+            {
+                throw new InvalidOperationException("PrintService is empty");
+            }
+            return _values[0];
+        }
+
+        public void Print() 
+        {
+            Console.Write("[");
+            for (int i = 0; i < _count - 1; i++)
+            {
+                Console.Write(_values[i] + ", ");
+            }
+            if (_count > 0) 
+            {
+                Console.Write(_values[_count - 1]);
+            }
+            Console.WriteLine("]");
+            
+        }
+
+    }
+}
---
+++ Generics/Generics/Program.cs
@@ -0,0 +1,35 @@
+﻿
+
+using Generics;
+
+//Definindo tipo de inteiro
+PrintService<int> printServiceInt = new PrintService<int>();
+
+Console.Write("How many values: ");
+int n = int.Parse(Console.ReadLine());
+
+for (int i = 0; i < n; i++) 
+{
+    int x = int.Parse(Console.ReadLine());
+    printServiceInt.AddValue(x);
+}
+
+printServiceInt.Print();
+Console.Write("First: " + printServiceInt.First());
+
+Console.WriteLine();
+
+//Definindo tipo de string
+PrintService<string> printServiceString = new PrintService<string>();
+
+Console.Write("How many values: ");
+int n1 = int.Parse(Console.ReadLine());
+
+for (int i = 0; i < n; i++) 
+{
+    string x1 = Console.ReadLine();
+    printServiceString.AddValue(x1);
+}
+
+printServiceString.Print();
+Console.Write("First: " + printServiceString.First());
