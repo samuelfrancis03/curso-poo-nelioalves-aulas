c424c617d5431e3cd9328761374ee00aea69552a
samuelfrancis03 <samuelfrancis03@hotmail.com>
2026-08-27 17:23:27 -0300

    Aula sobre Extension Methods

---
+++ TopicosEspeciais2/TopicosEspeciais2/Extensions/DateTimeExtensions.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using System.Globalization;
+
+namespace TopicosEspeciais2.Extensions
+{
+    static class DateTimeExtensions
+    {
+
+        //Classi para estender o metodo do DateTime, o que possibilita a chamada do metodo/função no propio objeto declarado na main
+        public static string ElapsedTime(this DateTime thisObj) 
+        {
+            TimeSpan duration = DateTime.Now.Subtract(thisObj);
+
+            if (duration.TotalHours < 24.0)
+            {
+                return duration.TotalHours.ToString("F1", CultureInfo.InvariantCulture) + " Hours";
+            }
+            else 
+            {
+                return duration.TotalDays.ToString("F1", CultureInfo.InvariantCulture) + " Days";
+            }
+        }
+
+    }
+}
---
+++ TopicosEspeciais2/TopicosEspeciais2/Extensions/StringExtensions.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace TopicosEspeciais2.Extensions
+{
+    static class StringExtensions
+    {
+
+        public static string Cut(this string thisObj, int count) 
+        {
+            if (thisObj.Length <= count)
+            {
+                return thisObj;
+            }
+            else 
+            {
+                return thisObj.Substring(0, count) + "...";
+            }
+        }
+
+    }
+}
