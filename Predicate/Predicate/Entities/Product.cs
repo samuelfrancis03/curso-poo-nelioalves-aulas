1f3be8d71e1d10bc3eb301c72879666843409965
samuelfrancis03 <samuelfrancis03@hotmail.com>
2026-09-03 15:40:19 -0300

    Aula sobre Predicate

---
+++ Predicate/Predicate/Entities/Product.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Predicate.Entities
+{
+    internal class Product
+    {
+        public string Name { get; set; }
+
+        public double Price { get; set; }
+
+        public Product(string name, double price)
+        {
+            Name = name;
+            Price = price;
+        }
+
+        public override string ToString()
+        {
+            return Name + " " + Price.ToString("F2");
+        }
+    }
+}
---
+++ Predicate/Predicate/Program.cs
@@ -0,0 +1,23 @@
+﻿
+
+using Predicate.Entities;
+
+List<Product> list = new List<Product>();
+list.Add(new Product("Tv", 900.00));
+list.Add(new Product("Mouse", 50.00));
+list.Add(new Product("Tablet", 350.50));
+list.Add(new Product("HD Case", 80.90));
+
+//Predicate é um delegate que recebe um metodo que recebe um objeto e retorna um valor boleano
+list.RemoveAll(ProductTest);
+
+foreach (Product p in list) 
+{
+    Console.WriteLine(p);
+}
+
+//Função que retorna o boleando para os produtos que tenha o preçe maior ou igual a cem.
+static bool ProductTest(Product p) 
+{
+    return p.Price >= 100.0;
+}
