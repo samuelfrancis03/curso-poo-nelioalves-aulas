bdd8d3992af0a66f2cf059f46123936bc19e169f
samuelfrancis03 <samuelfrancis03@hotmail.com>
2026-09-03 15:40:36 -0300

    Aula sobre Action

---
+++ Action/Action/Entities/Product.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Action.Entities
+{
+    internal class Product
+    {
+        public string Name { get; set; }
+
+        public double Price { get; set; }
+
+        public Product(string name, double price)
+        {
+            Name = name;
+            Price = price;
+        }
+
+        public override string ToString()
+        {
+            return Name + " " + Price.ToString("F2");
+        }
+
+    }
+}
---
+++ Action/Action/Program.cs
@@ -0,0 +1,38 @@
+﻿/*
+ Action =>  Representa um metédo void que recebe zero ou mais objetos.
+ */
+
+using Action.Entities;
+
+List<Product> list = new List<Product>();
+list.Add(new Product("Tv", 900.00));
+list.Add(new Product("Mouse", 50.00));
+list.Add(new Product("Tablet", 350.50));
+list.Add(new Product("HD Case", 80.90));
+
+
+
+list.ForEach(UpdatePrice);
+/*
+Outras versões de usar o Delegate Action
+
+Action<Product> act = UpdatePrice;
+list.ForEach(act);
+
+Action<Product> act = p => { p.Price += p.Price * 0.1;  };
+list.ForEach(act);
+
+list.ForEach(p => { p.Price += p.Price * 0.1;  });
+*/
+
+foreach (var item in list) 
+{
+    Console.WriteLine(item);
+}
+
+
+static void UpdatePrice(Product p) 
+{
+    p.Price += p.Price * 0.1;
+}
+
